b1c4c4ba1f216c8856c2e701ae558ec6f187d504
Hígor Fonseca <higfonseca@gmail.com>
2021-05-04 15:25:10 -0300

    feat: adding validations

--- validator/string.go
+++ validator/string.go
@@ -2,6 +2,7 @@ package validator
 
 import (
 	"os"
+	"strconv"
 )
 
 func (v *Validator) StringRequired(key string) string {
@@ -11,4 +12,39 @@ func (v *Validator) StringRequired(key string) string {
 	}
 
 	return val
+}
+
+func (v *Validator) stringOrDefault(key, def string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		return def
+	}
+
+	return val
+}
+
+func (v *Validator) boolOrDefault(key, def string) bool {
+	var ret bool
+	val := os.Getenv(key)
+	if val == "" {
+		ret, _ = strconv.ParseBool(def)
+		return ret
+	}
+
+	ret, _ = strconv.ParseBool(val)
+	return ret
+}
+
+func (v *Validator) intOrDefault(key string, def int) int {
+	val := os.Getenv(key)
+	if val == "" {
+		return def
+	}
+
+	num, err := strconv.Atoi(val)
+	if err != nil {
+		v.errs = append(v.errs, "unable to convert "+key+" to int")
+	}
+
+	return num
 }
