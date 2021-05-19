1560a337f61480dcf7001ad468fb79001d110e8b
Jessica Simplicio <jessicasimplicio1@gmail.com>
2021-05-19 11:45:36 -0300

    test: add test for env that isn't a boolean

--- test/envreader/bool_test.go
+++ test/envreader/bool_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	
 	"github.com/escaletech/go-escale/envreader"
+	"github.com/escaletech/go-escale/messages"
 	testUtils "github.com/escaletech/go-escale/test/utils"
 
 	"github.com/stretchr/testify/assert"
@@ -16,20 +17,32 @@ func TestBool(t *testing.T) {
 			t.Run("It should return the variable value and error's slice should be empty", func(t *testing.T) {
 				r := envreader.NewEnvReader(testUtils.GetFakeEnv)
 
-				response := r.BoolOrDefault(testUtils.FakeEnvVarNameBool, true)
+				response := r.BoolOrDefault(testUtils.FakeEnvVarNameBool, false)
 
 				assert.Equal(t, true, response)
 				assert.Equal(t, false, r.HasErrors())
 			})
 
-			t.Run("Env doesn't have the required value", func(t *testing.T) {
+			t.Run("Env doesn't have the required key", func(t *testing.T) {
 				t.Run("It should return a default value and error's slice should be empty", func(t *testing.T) {
 					r := envreader.NewEnvReader(testUtils.GetFakeEnv)
-	
+
 					response := r.BoolOrDefault("invalidEnvVarName", false)
-	
+
 					assert.Equal(t, false, response)
 					assert.Equal(t, false, r.HasErrors())
+				})
+			})
+
+			t.Run("Env has the required key but its value isn't a boolean", func(t *testing.T) {
+				t.Run("It should return false and an error message to the slice", func(t *testing.T) {
+					r := envreader.NewEnvReader(testUtils.GetFakeEnv)
+	
+					response := r.BoolOrDefault(testUtils.FakeEnvVarNameString, false)
+
+					assert.Equal(t, false, response)
+					assert.Equal(t, true, r.HasErrors())
+					assert.Equal(t, messages.UnableToConvertToBool(testUtils.FakeEnvVarNameString), r.Errs[0])
 				})
 			})
 		})
