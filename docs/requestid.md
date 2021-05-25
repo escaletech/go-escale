b9bd56f12e58e57be12f28b46fcdf6af9424af41
Hígor Fonseca <higfonseca@gmail.com>
2021-05-25 17:36:55 -0300

    feat: adding requestid package (#10)

---
+++ docs/requestid.md
@@ -0,0 +1,3 @@
+# requestid
+
+Middleware que adiciona uma Request ID no cabeçalho `X-Request-Id`, caso não exista.
---
+++ requestid/requestId.go
@@ -0,0 +1,28 @@
+package requestid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+type requestIDKey struct{}
+
+const requestIDHeader = "X-Request-Id"
+
+func Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, set(r))
+	})
+}
+
+func set(r *http.Request) *http.Request {
+	current := r.Header.Get(requestIDHeader)
+	if current == "" {
+		id, _ := uuid.NewRandom()
+		current = id.String()
+	}
+
+	return r.WithContext(context.WithValue(r.Context(), requestIDKey{}, current))
+}
