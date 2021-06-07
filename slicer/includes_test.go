8cdbaa558cfe34da69f0541a4dd673be3518156b
Hígor Fonseca <higfonseca@gmail.com>
2021-06-07 14:34:49 -0300

    refactor: validating if reference is a slice or array on func Includes() (#16)
    * feat: adding Includes to slicer package
    
    * test: moving tests to their packages
    
    * refactor: validating if reference is a slice or array on func Includes()
    
    * ci: adding release to production flow

--- slicer/includes_test.go
+++ slicer/includes_test.go
@@ -1,8 +1,10 @@
 package slicer_test
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/escaletech/go-escale/messages"
 	"github.com/escaletech/go-escale/slicer"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,42 +23,67 @@ func TestIncludes(t *testing.T) {
 	}
 
 	t.Run("The slice has the searched value", func(t *testing.T) {
-		t.Run("returns true (searched value is a struct)", func(t *testing.T) {
+		t.Run("returns true and no errors (searched value is a struct)", func(t *testing.T) {
 			refSlice := []operation{
 				operationA, operationB,
 			}
 
-			response := slicer.Includes(operationA, refSlice)
+			response, err := slicer.Includes(operationA, refSlice)
 
-			assert.Equal(t, true, response)
+			assert.True(t, *response)
+			assert.Nil(t, err)
 		})
 
-		t.Run("returns true (searched value is a string)", func(t *testing.T) {
+		t.Run("returns true and no errors (searched value is a string)", func(t *testing.T) {
 			refSlice := []string{"testando", "islaice"}
 
-			response := slicer.Includes("islaice", refSlice)
+			response, err := slicer.Includes("islaice", refSlice)
 
-			assert.Equal(t, true, response)
+			assert.True(t, *response)
+			assert.Nil(t, err)
+		})
+
+		t.Run("returns true and no errors (reference is an array)", func(t *testing.T) {
+			var ref [2]string
+			ref[0] = "abc"
+			ref[1] = "def"
+
+			response, err := slicer.Includes("def", ref)
+
+			assert.True(t, *response)
+			assert.Nil(t, err)
 		})
 	})
 
 	t.Run("The slice doesn't have the searched value", func(t *testing.T) {
-		t.Run("returns false (searched value is a struct)", func(t *testing.T) {
+		t.Run("returns false and no errors (searched value is a struct)", func(t *testing.T) {
 			refSlice := []operation{
 				operationA,
 			}
 
-			response := slicer.Includes(operationB, refSlice)
+			response, err := slicer.Includes(operationB, refSlice)
 
-			assert.Equal(t, false, response)
+			assert.False(t, *response)
+			assert.Nil(t, err)
 		})
 
-		t.Run("returns false (searched value is a string)", func(t *testing.T) {
+		t.Run("returns false and no errors (searched value is a string)", func(t *testing.T) {
 			refSlice := []string{"testando"}
 
-			response := slicer.Includes("islaice", refSlice)
+			response, err := slicer.Includes("islaice", refSlice)
 
-			assert.Equal(t, false, response)
+			assert.False(t, *response)
+			assert.Nil(t, err)
+		})
+	})
+
+	t.Run("Reference is not a slice or an array", func(t *testing.T) {
+		t.Run("returns nil with an error for \"Reference must be a slice or an array\"", func(t *testing.T) {
+			ref := "istringue"
+			response, err := slicer.Includes("tantofaz", ref)
+
+			assert.Nil(t, response)
+			assert.Equal(t, errors.New(messages.RefNotSliceOrArray), err)
 		})
 	})
 }
