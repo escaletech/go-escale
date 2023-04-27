cb9dd8e52ef4fa063756a38ed7a00bfb7db7f659
Felipe Colussi-oliva <66074971+felipe-colussi@users.noreply.github.com>
2023-04-27 09:33:00 -0300

    refactor: add queryParams to Request (#44)
    * refactor: add pathParams to Request
    
    * fix: correct the name

---
+++ httpclient/httpClient_test.go
@@ -0,0 +1,55 @@
+package httpclient
+
+import "testing"
+
+func Test_castQueryParamsToString(t *testing.T) {
+	type args struct {
+		pathParams map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "ok with 1",
+			args: args{
+				pathParams: map[string]string{
+					"path1": "um",
+				},
+			},
+			want: "?path1=um",
+		},
+		{
+			name: "ok with 2",
+			args: args{
+				pathParams: map[string]string{
+					"path1": "um",
+					"path2": "dois",
+				},
+			},
+			want: "?path1=um&path2=dois",
+		},
+		{
+			name: "no map",
+			args: args{
+				pathParams: nil,
+			},
+			want: "",
+		},
+		{
+			name: "empty map",
+			args: args{
+				pathParams: map[string]string{},
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := castQueryParamsToString(tt.args.pathParams); got != tt.want {
+				t.Errorf("castQueryParamsToString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
